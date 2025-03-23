a7b685206695df873745acef891cbd1942f185f0
Jon Amelibia <jon.amelibia@alumni.mondragon.edu>
2025-03-23 20:56:43 +0100

    Index page created

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -32,8 +32,8 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path('/', RedirectView.as_view(url='/catalog/', permanent=True)),
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
-    path('/', RedirectView.as_view(url='/catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
