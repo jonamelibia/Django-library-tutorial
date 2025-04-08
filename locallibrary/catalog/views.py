6a6dacd847f9ab8c31dee529d49854f4145b538d
Jon Amelibia <jon.amelibia@alumni.mondragon.edu>
2025-04-08 21:37:57 +0200

    authors page added

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -23,3 +23,37 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+
+from django.views import generic
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 1 
+    def get_context_data(self, **kwargs):
+        # Llame primero a la implementación base para obtener un contexto.
+        context = super(BookListView, self).get_context_data(**kwargs)
+        # Obtenga el blog del id y agréguelo al contexto.
+        context['some_data'] = 'Estos son solo algunos datos'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 1 
+
+    def get_context_data(self, **kwargs):
+        # Llame primero a la implementación base para obtener un contexto.
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        
+        # Agregar un diccionario con los libros de cada autor
+        authors = context['object_list']  # Lista de autores en la página actual
+        author_books = {author: author.book_set.all() for author in authors}
+        
+        # Agregar los libros al contexto
+        context['author_books'] = author_books
+        return context
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
